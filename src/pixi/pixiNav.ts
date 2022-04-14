2560fffd9f4f9dfd84bb9801d45253cd3fed3c89
Ethan Arns <ethanarns95@gmail.com>
2022-04-13 20:40:03 -0700

    Fully clamped navigation

--- src/pixi/pixiNav.ts
+++ src/pixi/pixiNav.ts
@@ -9,7 +9,7 @@ export const ZOOM_SCALE: Record<number,number> = {
     2: 1.5,
     3: 2.0
 };
-export const MAX_ZOOM_KEY = 3;
+export const MAX_ZOOM_KEY = Object.values(ZOOM_SCALE)[Object.values(ZOOM_SCALE).length - 1];
 
 let zoom_global_key = 1;
 
@@ -79,6 +79,13 @@ function clampNav(navObject: DisplayObject): void {
     }
     if (newCoords.y < 0) {
         navObject.pivot.y = 0 + (CANVAS_HEIGHT / scaleVal / 2);
+    }
+
+    if (newCoords.x + CANVAS_WIDTH / scaleVal > 0xff*16) {
+        navObject.pivot.x = 0xff*16 - (CANVAS_WIDTH / scaleVal / 2);
+    }
+    if (newCoords.y + CANVAS_HEIGHT / scaleVal > (0xff*16 / 2)) {
+        navObject.pivot.y = (0xff*16 / 2) - (CANVAS_HEIGHT / scaleVal / 2);
     }
 }
 
